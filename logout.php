8646b1cf3140297b6575534051305c0bf0762b10
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-18 14:31:22 +0700

    feat(logout): add logout feature

--- logout.php
+++ logout.php
@@ -1,3 +1,11 @@
 <?php
+include('./utils/auth.php');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+  Auth::logout();
+}
+
 header("Location: /login.php");
 ?>
--- utils/auth.php
+++ utils/auth.php
@@ -1,9 +1,15 @@
 <?php
-session_start();
 class Auth {
   public static function login($userId) {
+    session_start();
     $_SESSION['login'] = true;
     setcookie('user', $userId);
+  }
+
+  public static function logout() {
+    session_destroy();
+    setcookie('PHPSESSID');
+    setcookie('user'); 
   }
 }
 ?>
