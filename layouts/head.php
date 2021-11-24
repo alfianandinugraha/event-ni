50131a3e1978daa0b3cdd2e5f484072c2dee9f71
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-24 08:16:23 +0700

    refactor(materializecss): use cdn and remove local file

--- layouts/head.php
+++ layouts/head.php
@@ -1,12 +1,12 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/public/css/materialize.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@materializecss/materialize@1.1.0-alpha/dist/css/materialize.min.css">
+<script src="https://cdn.jsdelivr.net/npm/@materializecss/materialize@1.1.0-alpha/dist/js/materialize.min.js"></script>
 <link rel="stylesheet" href="/public/css/style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.3.3/purify.min.js"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script src="/public/js/materialize.min.js"></script>
 <style>
 
 .container {
